d15db082762095b533c226320eb21c8aa8c14a74
opal pinchas <opalp10145@gmail.com>
2022-04-19 18:16:47 +0300

    add api key

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAeKOoG_NQYUlxuW1lvGe3uzKwZyQPR3Ow'; //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
